f9796f8b2530f60bbf44ddcb5719e33de2c641a4
shantanu das <shan@shantanus-MacBook-Air.local>
2020-01-11 13:58:12 +0530

    InitialCommit for Azure TF Resource gROUP

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "azurerg" {
+  value = "nameoftheresourcegroup-${azurerm_resource_group.vmrg.name}-at${formatdate("DD MMM YYYY hh:mm ZZZ",timestamp())}"
+}
+#
+output "azurerg1" {
+  value = "id-${azurerm_resource_group.vmrg.id}"
+}
---
+++ var.tf
@@ -0,0 +1,25 @@
+variable "account_tier" {
+  default = "Standard_ZRS"
+}
+variable "location" {
+  #default = ""
+}
+
+
+variable "vm_hostname" {
+  default = "host"
+}
+
+variable "rgname" {
+#  #default = ""
+}
+
+
+variable "tags" {
+  type = map 
+  default={
+owner = "shan"
+resource = "storage"
+another = "tag"
+  }
+}
