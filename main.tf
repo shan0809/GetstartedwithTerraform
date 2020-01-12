c36f9485186f25b537b093978902deb2ac820917
shantanu das <shan@shantanus-MacBook-Air.local>
2020-01-12 12:25:17 +0530

    demo

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "random_id" "vm-sa" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = "bootdsk${lower(random_id.vm-sa.hex)}"
-          resource_group_name      = azurerm_resource_group.vmrg.name
+  resource_group_name      = azurerm_resource_group.vmrg.name
             location                 = azurerm_resource_group.vmrg.location
                   account_tier             = element(split("_", var.account_tier), 0)
   account_replication_type = element(split("_", var.account_tier), 1)
